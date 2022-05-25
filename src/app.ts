e0f9533afd9c6715e5d87ad0a0fecfe82975b926
Arthur Samuel <arthursamuel1696@gmail.com>
2022-05-25 13:49:37 +0800

    allow cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 const app = express();
+const cors = require('cors')
 const bodyParser = require("body-parser");
 const loginRouter = require('./routes/login/RouteLogin')
 const privateRouter = require('./routes/private/RoutePrivate')
 const publicRouter = require('./routes/public/RoutePublic')
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
